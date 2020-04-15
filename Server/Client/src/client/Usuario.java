73d72b9f2122053161ddbdba0bad042c21fb5977
Ezequiel <ezequiel.fritz@hotmail.com>
2020-04-15 15:03:25 -0300

    Add server app to repo

---
+++ Server/Client/src/client/Usuario.java
@@ -0,0 +1,25 @@
+package client;
+
+public class Usuario {
+    private String nombre;
+    private String numeroDeIP;
+    private boolean estado;
+
+    public Usuario(String nombre, String numeroDeIP, boolean estado) {
+        this.nombre = nombre;
+        this.numeroDeIP = numeroDeIP;
+        this.estado = estado;
+    }
+    
+    public String getNumeroDeIP() {
+        return numeroDeIP;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
