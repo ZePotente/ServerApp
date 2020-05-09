ad790a6f14e0a11e584f805ee46a0f858132c710
ZePotente <ezeposada@gmail.com>
2020-05-09 12:35:35 -0300

    Nueva app.

---
+++ Server/Client/src/client/Server.java
@@ -0,0 +1,68 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+
+import java.io.ObjectOutputStream;
+
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.net.UnknownHostException;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Server {
+    private static ServerSocket server;
+    private static HashMap<String, Usuario> usuarios = new HashMap<>();
+    private static FormateadorListaUsuarios formateador = new FormateadorListaUsuarios();
+    
+    public Server() {
+        super();
+    }
+    
+    public void abrirSv(int port) throws IOException, ClassNotFoundException {
+        new Thread() {
+            public void run() {
+                try {
+                    server = new ServerSocket(port);
+                    while (true) {
+                        Socket socket = server.accept();
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader objectIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String aux;
+                        String identificador = objectIn.readLine();
+                        if (identificador.equalsIgnoreCase("AvisoConexion")) {
+                            // Leo el nombre del usuario
+                            aux = objectIn.readLine();
+                            Usuario usuario = usuarios.get(aux);
+                            // Ya esta agregado, solo debo modificar su estado
+                            if (usuario != null) {
+                                // Leo la IP del usuario que en este caso no se utiliza
+                                aux = objectIn.readLine();
+                                // Leo el estado del usuario
+                                aux = objectIn.readLine();
+                                usuario.setEstado(aux.equalsIgnoreCase("true"));
+                            } else { // No esta agregado, debo agregarlo
+                                usuarios.put(aux, new Usuario(aux, objectIn.readLine(), true));
+                            }
+                        } else if (identificador.equalsIgnoreCase("RequestReceptores")) {
+                            out.println(formateador.escribeListUsuarios(usuarios));
+                            out.flush();
+                        }
+                        out.close();
+                        socket.close();
+                        }
+                    } catch (IOException e) {
+                    }
+                }
+        }.start();
+    }
+}
+
---
+++ Server/Client/src/client/Sistema.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.IOException;
+
+public class Sistema {
+    private static Sistema sistema = null;
+    
+    private final int port = 100;
+    private Server sv = null;
+    
+    public static synchronized Sistema getInstance() {
+        if (sistema == null) {
+            sistema = new Sistema();
+        }
+        return sistema;
+    }
+    
+    private Sistema() {
+        sv = new Server();
+        iniciarSv();
+    }
+    
+    private void iniciarSv() {
+        try {
+            sv.abrirSv(port);
+        } catch (ClassNotFoundException | IOException e) {
+        }
+    }
+}
+
