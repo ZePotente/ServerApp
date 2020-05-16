455ce52e3831af9631d1eb60fae9e5f67a7bd247
ZePotente <ezeposada@gmail.com>
2020-05-16 19:22:56 -0300

    Detalle de javadoc.

--- Server/Client/src/echo/Conexiones.java
+++ Server/Client/src/echo/Conexiones.java
@@ -54,7 +54,7 @@ public class Conexiones {
      * <b>Pre:</b> nombres != null. (creo, no se que pasa en el for si es null.<br>
      * Aplica desconectar(String nombre) a cada elemento de la coleccion.<br>
      * 
-     * @param conexiones Lista de nombres a cortarles la conexion.
+     * @param nombres Lista de nombres a cortarles la conexion.
      */
     public void desconectar(ArrayList<String> nombres) {
         for(String con : nombres)
